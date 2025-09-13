d2e5f8f7b45be5b12d6e32e7178114f448c93823
jdy7149 <184757480+jdy7149@users.noreply.github.com>
2025-09-14 00:47:09 +0900

    Add main image

--- src/home.js
+++ src/home.js
@@ -1,3 +1,5 @@
+import mainImage from './img/main.jpg';
+
 export default function () {
     const contentDiv = document.querySelector('#content');
 
@@ -8,7 +10,16 @@ export default function () {
     title.textContent = 'Welcome to My Restaurant';
 
     const p = document.createElement('p');
-    p.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fermentum, felis sit amet hendrerit lobortis, quam arcu facilisis ipsum, ac luctus nisi risus nec magna. Donec mollis mollis mauris, in ultrices dolor elementum congue. Maecenas egestas, quam vel eleifend feugiat, tortor quam sodales urna, in fringilla velit eros a arcu.'
+    
+    const img = document.createElement('img');
+    img.src = mainImage;
+    img.alt = 'main-img';
+    img.width = 650;
+    img.height = 400;
+    p.appendChild(img);
+
+    const pText = document.createTextNode('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fermentum, felis sit amet hendrerit lobortis, quam arcu facilisis ipsum, ac luctus nisi risus nec magna. Donec mollis mollis mauris, in ultrices dolor elementum congue. Maecenas egestas, quam vel eleifend feugiat, tortor quam sodales urna, in fringilla velit eros a arcu.');
+    p.appendChild(pText);
 
     container.appendChild(title);
     container.appendChild(p);
--- webpack.config.js
+++ webpack.config.js
@@ -26,9 +26,13 @@ module.exports = {
                 use: ['style-loader', 'css-loader'],
             },
             {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource',
-            }
+            },
         ],
     },
 };
